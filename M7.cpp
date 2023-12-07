3751fe563538e1df1c14bba3d7c5263ece6206a5
hawexrutile <alan.john@niser.ac.in>
2023-12-07 16:54:02 +0530

    Renamed to M7

--- M7.cpp
+++ M7.cpp
@@ -1,17 +1,5 @@
-//? Added comments
-//? 3D-> 2D
-//? Added Vission
-//? Moved vision to a new function
-//? Added gif maker
-//? Added break() wherever divission occurs
-//? changed round() -> ceil() in particlesPerDimension()
-//? Changed break -> continue
-//? Added more vaeriables to tensor.json
-//? Made global parameters
-//? Move vission cone into calculateForcesAndEnergy for faster calculations
-//? removed noise fromm particle struct and added it to a vector
-//? Moved the plotting functions into a different file
-//? Added comments
+//added auto params
+// Changed saving method to folders based on excecution time
 #define _USE_MATH_DEFINES
 
 #include <iostream>
@@ -23,6 +11,17 @@
 #include <ctime>
 #include <random>
 #include <string>
+#include <algorithm>
+#include <string>
+#ifdef _WIN32
+//compiled on a Windows system
+    #include <direct.h>
+#else
+//compiled on a Unix-based system
+    #include <sys/stat.h>
+#endif
+#include <sys/types.h>
+#include <omp.h>
 #include "json.hpp" // Include the JSON library, e.g., nlohmann/json
 
 using namespace std;
@@ -31,28 +30,44 @@ using namespace std;
     //version manger parameters
     int codeVersion=4;
     int currentVersion=0;
-    string comment=" Changed theta";
+    string comment=" Trying for a huge number of particle run, for the displasment and persistence plots, as they are not being exactly logaraithmic, also for the prev run I forgot to change the parameters";
     // Simulation parameters
     double epsilon = 1.0;               // Depth of the potential well
     double sigma = 1.0;                 // Distance at which the potential is zero
-    int numParticles = 100;             // !Number of particles          100   625  300
-    double boxSize = 50.0;              // !Size of the simulation box   12   250  175
+    int numParticles = 10;             // !Number of particles          10 100 625 5000 10,000
+    double boxSize = 32.0;              // !Size of the simulation box   32 100 250 700  1000
     double temperature = 1.0;           // !Temperature to be fed to MB Distribution
-    double my_gamma=100.0;              // ! Gamma ;
     double cutoff = 1.12246205 * sigma; // Cutoff distance for the Lennard-Jones potential (sigma*2^(1/6)=1.122462048)
+    double my_gamma=100.0;              // ! Gamma ;
     //Dynamic Parameters
     double timestep = 0.001;            // !Timestep
-    int numSteps = 100000;              // !Number of simulation steps
+    int numSteps = 1000000;              // !Number of simulation steps
     int dataCompression=500;            // If dataCompression-n; The KE & PE at every nth timestep is writen in .dat file
     int period=100;                     // No of time steps to skip before applying next velocity initialization based on MB Distribution
-    int NoOfPeriods=100;                 // No of time velocity intialization is to be applied
+    int NoOfPeriods=1000;                 // No of time velocity intialization is to be applied
     //Vission parameters
-    double Omega = 4.8;                 // Maneuverability strength (60 * D_R)
-    double D_R = 0.08;                  // Rotational diffusion coefficient (fixed as such) 0.08
-    double R_0 = 1.5;                   // Characteristic length      (1.5 * sigma)
     double theta = M_PI/7.0;            // Half of the opening angle of the vision cone (between pi/12 and pi/2)
+    double R_0 = 1.5 * sigma;
+    double D_R = 0.08;                  // Rotational diffusion coefficient (fixed as such) 0.08
+    double D_T= temperature/my_gamma;
+    double Pe=(epsilon/temperature)-1;
     double v_0=1.0;                     //(such that Pe= sigma* v_0/D_T=2000
+    double Omega = 4.8;                 // Maneuverability strength (60 * D_R)
+    //Parellelization parameters
+    int numThreads = 8;  // Set the desired number of threads for paralaization
+
 //params-end
+//autoset-params-begin
+    // double tau=sqrt(sigma*sigma/temperature);
+    // double my_gamma=100/tau;
+    // double D_R=0.08/tau;
+    // double D_T= temperature/my_gamma;
+    // double Pe=(epsilon/temperature)-1;
+    // double v_0=Pe * D_T / sigma;
+    // double Omega=62.5*D_R;
+//autoset-params-end
+
+
 // Particle structure
 struct Particle {
     double x, y, vx, vy, ax, ay, phi, PE;
@@ -75,7 +90,8 @@ void LJandVissionCone(vector<Particle>& particles, double L, double dt) {
     std::vector<double> vNc(numParticles, 0.0);
     std::vector<double> vsum_term(numParticles, 0.0);
 
-    // Calculate forces and potential energy between particles
+    // Calculate forces and potential energy between particles (parallelized)
+    #pragma omp parallel for
     for (int i = 0; i < particles.size(); ++i) {
         double phi_i = particles[i].phi;
         for (int j = i + 1; j < particles.size(); ++j) {
@@ -115,7 +131,7 @@ void LJandVissionCone(vector<Particle>& particles, double L, double dt) {
             double phi_ij = atan2(dy, dx);
             double phi_ji = atan2(-dy, -dx);
 
-            if (r <= 4 * R_0) {
+            if (r <= 4.0 * R_0) {
                 // Check if particles are within the vision cone of each other
                 if (((dx * cos(phi_i)) + (dy * sin(phi_i))) / r >= cos(theta)) {
                     double delta_phi = phi_ij - phi_i;
@@ -129,6 +145,7 @@ void LJandVissionCone(vector<Particle>& particles, double L, double dt) {
                 }
             }
         }
+        
 
         // Update particle orientation (phi) and angular acceleration (ax, ay)
         double dphi_i;
@@ -139,6 +156,7 @@ void LJandVissionCone(vector<Particle>& particles, double L, double dt) {
         particles[i].ax += my_gamma * v_0 * cos(particles[i].phi); // Update ax
         particles[i].ay += my_gamma * v_0 * sin(particles[i].phi); // Update ay
     } 
+    #pragma omp barrier
     // Calculate and store the average potential energy per particle for a given timestep in the data set of the last particle in the timestep
     particles[numParticles - 1].PE = totalPotentialEnergy / particles.size();
 }
@@ -284,50 +302,13 @@ void saveParticleData( std::vector<std::vector<std::vector<double>>>& tensor, st
         tensor[jsonStep][p][1] = particles[p].y; // y-coordinate
         tensor[jsonStep][p][2] = particles[p].vx; // x-velocity
         tensor[jsonStep][p][3] = particles[p].vy; // y-velocity
-        tensor[jsonStep][p][4] = particles[p].phi; // Vission Cone direction
+        tensor[jsonStep][p][4] = particles[p].phi; // Vission Cone direction 
         if (p==numParticles-1) tensor[jsonStep][p][5] = particles[p].PE; // y-velocity
     }
 
-    // Open the file in text mode
-    std::ofstream file("particle_positions.json");
 
-    if (file.is_open()) {
-        // Serialize the tensor to JSON
-        nlohmann::json jsonData(tensor);
-
-        // Write the JSON data to the file
-        file << jsonData.dump(4); // The argument sets the indentation for readability
-
-        // Close the file
-        file.close();
-    } else {
-        std::cerr << "Unable to open the JSON file for writing." << std::endl;
-    }
 }
 
-int versionUpdater(){
-    int currentPlotVersion = currentVersion;
-    std::ifstream versionNumber("version.dat"); // Open the file for reading
-    if (versionNumber.is_open()) {
-        versionNumber >> currentPlotVersion; // Read the current integer from the file
-        versionNumber.close();
-
-        // Increment the integer by one
-        int newVersion = currentPlotVersion + 1;
-
-        std::ofstream versionNumberOut("version.dat"); // Open the file for writing
-        if (versionNumberOut.is_open()) {
-            versionNumberOut << newVersion; // Write the new integer to the file
-            versionNumberOut.close();
-            std::cout << "Updated version: " << newVersion << std::endl;
-        } else {
-            std::cerr << "Unable to open version file for writing." << std::endl;
-        }
-    } else {
-        std::cerr << "Unable to open version file for reading." << std::endl;
-    }
-    return currentPlotVersion;
-}
 
 
 void Notifier(int codeVersion, int currentVersion){
@@ -336,8 +317,70 @@ void Notifier(int codeVersion, int currentVersion){
     system(cd.c_str());
 }
 
+void createDirectory(const std::string& dir) {
+    // program is being compiled on a Windows system
+    #ifdef _WIN32
+    //_mkdir function from the direct.h library to create the directory
+        _mkdir(dir.c_str());
+    // compiled on a Unix-based system
+    #else
+    // mkdir function from the sys/stat.h library to create the directory. The second argument 0777 sets the permissions of the directory 
+        mkdir(dir.c_str(), 0777);
+    #endif
+}
+void save_param(string location){
+    // save the above variables as a key value pair in a csv file
+    ofstream myfile;
+    myfile.open (location+"/params.csv");
+    myfile << "comment,"<<comment<<"\n";
+    myfile << "epsilon,"<<epsilon<<"\n";
+    myfile << "sigma,"<<sigma<<"\n";
+    myfile << "numParticles,"<<numParticles<<"\n";
+    myfile << "boxSize,"<<boxSize<<"\n";
+    myfile << "temperature,"<<temperature<<"\n";
+    myfile << "cutoff,"<<cutoff<<"\n";
+    myfile << "my_gamma,"<<my_gamma<<"\n";
+    myfile << "timestep,"<<timestep<<"\n";
+    myfile << "numSteps,"<<numSteps<<"\n";
+    myfile << "dataCompression,"<<dataCompression<<"\n";
+    myfile << "period,"<<period<<"\n";
+    myfile << "NoOfPeriods,"<<NoOfPeriods<<"\n";
+    myfile << "theta,"<<theta<<"\n";
+    myfile << "R_0,"<<R_0<<"\n";
+    myfile << "D_R,"<<D_R<<"\n";
+    myfile << "D_T,"<<temperature/my_gamma<<"\n";
+    myfile << "Pe,"<<(epsilon/temperature)-1.0<<"\n";
+    myfile << "v_0,"<<v_0<<"\n";
+    myfile << "Omega,"<<Omega<<"\n";
+    myfile.close();
+}
+
+string dirlocater(){
+    // Get the current time
+    time_t now = time(0);
+
+    // Allocate a buffer for the formatted time
+    char dt[100];  // Adjust the size based on your formatting needs
+
+    // Format the time using strftime
+    strftime(dt, sizeof(dt), "%Y_%m_%d_%H_%M_%S", localtime(&now));
+
+    // Convert ctime output to string and remove newline character
+    std::string str_dt(dt);
+    str_dt.erase(std::remove(str_dt.end()-1, str_dt.end(), '\n'), str_dt.end());
+
+    // Replace spaces and colons with underscores
+    std::replace(str_dt.begin(), str_dt.end(), ' ', '_');
+    std::replace(str_dt.begin(), str_dt.end(), ':', '_');
+    
+    // Create directory
+    createDirectory("Runs/"+str_dt);
+
+    return "Runs/"+str_dt;
+}
 
 int main() {
+    omp_set_num_threads(numThreads);
     // system("python reg.py");
     vector<Particle> particles(numParticles);
     vector<double> velocities;  // Empty velocity vector
@@ -352,13 +395,9 @@ int main() {
     //Calculating Packing fraction
     double packingFraction = (numParticles*M_PI*(sigma/2)*(sigma/2))/(boxSize*boxSize);
     cout << "Packing Fraction is: " <<packingFraction<<endl;
-    
-    // Open the output file for writing the data
-    ofstream outputFile("energy_data.dat");
-    if (!outputFile.is_open()) {
-        cerr << "Unable to open output file." << endl;
-        return 1;
-    }
+
+    string location = dirlocater();
+    save_param(location);
     // Main simulation loop
     for (int step = 0; step < numSteps; ++step) {
         // Update positions and velocities
@@ -373,19 +412,30 @@ int main() {
             // Write the position data to the output file
             saveParticleData(tensor, particles, step);
         }
-        if (fmod((1000.0 * step) / (numSteps * 1.0), 1.0) == 0.0) {
+        if (fmod((10000.0 * step) / (numSteps * 1.0), 1.0) == 0.0) {
             cout <<"  "<<(100.0 * step) / (numSteps*1.0) << " Percentage completed \r";
         }
     }
-    // Close the output file
-    outputFile.close();
+    // Open the file in text mode
+    std::ofstream file(location+"/particle_positions.json");
+
+    if (file.is_open()) {
+        // Serialize the tensor to JSON
+        nlohmann::json jsonData(tensor);
+
+        // Write the JSON data to the file
+        file << jsonData.dump(4); // The argument sets the indentation for readability
+
+        // Close the file
+        file.close();
+    } else {
+        std::cerr << "Unable to open the JSON file for writing." << std::endl;
+    }
 
     // Print a success message
     cout << "Simulation completed successfully. Starting the Plotting and Notifying engines" << endl;
 
 
-    // finalPositionRecorder(particles);
-    currentVersion=versionUpdater();
     // Notifier(codeVersion, currentVersion);
 
     return 0;
